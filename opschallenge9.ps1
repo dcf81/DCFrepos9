0668465760dc0b04f0adc3b2fb9e5d575a42a008
Chris <dcf1117@yahoo.com>
2023-01-18 19:57:36 -0600

    opschallenge9.ps1

--- opschallenge9.ps1
+++ opschallenge9.ps1
@@ -1,3 +1,7 @@
 $Begin = Get-Date -Date '12/22/2022 00:00:00'
 $End = Get-Date -Date '12/22/2022 23:59:59'
 Get-EventLog -LogName System -EntryType Error -After $Begin -Before $End > /home/chris/Desktop/last_24.txt
+Get-Eventlog -LogName System -InstanceID 16
+Get-Eventlog -LogName System -Newest 20
+Get-Eventlog -LogName System -Newest 500
+Get-Process -Name iexplore | Format-List
